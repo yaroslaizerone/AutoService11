e2b6dcc49771f753cd100f6cb8f7764a1d1904a9
ярослав Колпаков <kolpackov.yarosl@yandex.ru>
2023-09-25 17:40:35 +0700

    Создание структуры проекта.

--- AutoService11/App.xaml.cs
+++ AutoService11/App.xaml.cs
@@ -8,9 +8,6 @@ using System.Windows;
 
 namespace AutoService11
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
     }
--- AutoService11/Windows/MainClientWindow.xaml.cs
+++ AutoService11/Windows/MainClientWindow.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace AutoService11
+namespace AutoService11.Windows
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainClientWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainClientWindow : Window
     {
-        public MainWindow()
+        public MainClientWindow()
         {
             InitializeComponent();
         }
